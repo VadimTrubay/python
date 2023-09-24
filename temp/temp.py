c7862fea8d956dfebe373eaa15f6977cd91af77e
VadimTrubay <vadnetvadnet@ukr.net>
2023-09-24 21:43:03 +0300

    Update temp.py

--- temp/temp.py
+++ temp/temp.py
@@ -6,8 +6,25 @@
 #     count += 1
 # print(summ)
 
-def add(a, b, c):
-    print(f'Add result equals {a + b + c}')
+# def add(a, b, c):
+#     print(f'Add result equals {a + b + c}')
+
+
+user_input = int(input('enter n: '))
+res = 1
+for i in range(1, user_input+1):
+    res *= i
+print(res)
+
+
+
+
+
+
+
+
+
+
 
 
 
