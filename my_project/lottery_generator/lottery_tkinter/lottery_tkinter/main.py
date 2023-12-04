47cf5432ae8d4bff3e829860b658e78b9ab4dba6
VadimTrubay <vadnetvadnet@ukr.net>
2023-12-05 00:04:09 +0200

    commit

--- my_project/lottery_generator/lottery_tkinter/lottery_tkinter/main.py
+++ my_project/lottery_generator/lottery_tkinter/lottery_tkinter/main.py
@@ -19,51 +19,58 @@ def main():
         # Ask the user if they want to run again
         response = askyesno("Generate Again", "Do you want to generate again?")
         if response:
-            clear()
-            return
+            return True
         else:
-            pass
+            return False
 
     def get_selected_ball(event):
         global selected_ball
         selected_ball = int(combobox_ball.get())
         print(selected_ball)
 
+
     def get_selected_all_ball(event):
         global selected_all_ball
         selected_all_ball = int(combobox_all_ball.get())
         print(selected_all_ball)
 
+
     def get_balls():
-        global new_list
-        new_list = []
-        numbers = list(range(1, selected_all_ball + 1))
-        column = 0
-        for _ in numbers[:selected_ball]:
-            num = random.choice(numbers)
-            b = Button(text=f"{num}", bd=2, font=('Arial', 20, 'bold'), fg='#446600')
-            b.grid(row=7, column=column, padx=5, pady=5)
-            column += 1
-            new_list.append(num)
-            numbers.remove(num)
-            random.shuffle(numbers)
-            root.update()
-            time.sleep(1)
-        try_again()
+        answer = try_again()
+        if answer:
+            global new_list
+            new_list = []
+            numbers = list(range(1, selected_all_ball + 1))
+            column = 0
+            label = Label(text="random", font=('Arial', 10, 'bold'), fg='#996600')
+            label.grid(row=7, column=0, padx=10, pady=10)
+            for _ in numbers[:selected_ball]:
+                num = random.choice(numbers)
+                b = Button(text=f"{num}", bd=2, font=('Arial', 20, 'bold'), fg='#446655', width=3, height=1)
+                b.grid(row=8, column=column, padx=5, pady=5)
+                column += 1
+                new_list.append(num)
+                numbers.remove(num)
+                random.shuffle(numbers)
+                root.update()
+                time.sleep(1)
 
     def sort_balls():
         new_list.sort()
         column = 0
+        label = Label(text="sort", font=('Arial', 10, 'bold'), fg='#996600')
+        label.grid(row=9, column=0, padx=10, pady=10)
         for num in new_list:
-            b = Button(text=f"{num}", bd=2, font=('Arial', 20, 'bold'), fg='#006600')
-            b.grid(row=9, column=column, padx=5, pady=5)
+            b = Button(text=f"{num}", bd=2, font=('Arial', 20, 'bold'), fg='#006655', width=3, height=1)
+            b.grid(row=10, column=column, padx=5, pady=5)
             column += 1
             root.update()
             time.sleep(1)
-        try_again()
+
 
     def clear():
         root.destroy()
+
 
     def exiting():
         global running
@@ -74,24 +81,24 @@ def main():
     root.title("lottery_tkinter")
     # root.iconbitmap(default="favicon.ico")
     root.resizable(False, False)
-    root.geometry("600x500+600+100")
+    root.geometry("600x400+300+100")
     root.update_idletasks()
     # устанавливаем тему "classic"
     ttk.Style().theme_use("clam")
 
-    label = Label(text="lottery_tkinter")
+    label = Label(text="lottery")
     label.grid(row=0, column=0, padx=10, pady=10)
 
-    button_start = Button(text="Start", command=get_balls, bd=5, font=('Arial', 10, 'bold'))
+    button_start = Button(text="Start", command=get_balls, bd=5, font=('Arial', 10, 'bold'), fg='green')
     button_start.grid(row=1, column=0, padx=10, pady=10)
 
-    button_start = Button(text=" Sort ", command=sort_balls, bd=5, font=('Arial', 10, 'bold'))
+    button_start = Button(text=" Sort ", command=sort_balls, bd=5, font=('Arial', 10, 'bold'), fg='#774400')
     button_start.grid(row=1, column=1, padx=10, pady=10)
 
-    button_clear = Button(text="Clear", command=clear, bd=5, font=('Arial', 10, 'bold'))
+    button_clear = Button(text="Clear", command=clear, bd=5, font=('Arial', 10, 'bold'), fg='blue')
     button_clear.grid(row=1, column=2, padx=10, pady=10)
 
-    button_exiting = Button(text=" Exit ", command=exiting, bd=5, font=('Arial', 10, 'bold'))
+    button_exiting = Button(text=" Exit ", command=exiting, bd=5, font=('Arial', 10, 'bold'), fg='red')
     button_exiting.grid(row=1, column=3, padx=10, pady=10)
 
     label_choose_ball = ttk.Label(text="ball", font=('Arial', 10, 'bold'))
@@ -120,10 +127,6 @@ def main():
                                      textvariable=values_all_var)
     combobox_all_ball.grid(row=2, column=3, padx=10, pady=10)
     combobox_all_ball.bind("<<ComboboxSelected>>", get_selected_all_ball)
-    print(selected_ball)
-    print(selected_all_ball)
-
-
 
     root.mainloop()
 
